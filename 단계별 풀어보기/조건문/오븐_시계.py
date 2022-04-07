3b4f506181f954280038bd3311729b5e6a7000e5
ahnrh951019@naver.com <ahnrh951019@naver.com>
2022-04-08 00:34:06 +0900

    I solve the oven timer

--- 단계별 풀어보기/조건문/오븐_시계.py
+++ 단계별 풀어보기/조건문/오븐_시계.py
@@ -1,25 +1,21 @@
-import sys
-from unittest import case
-
 a, b = map(int, input().split(' '))
 if a in range(0,26) and b in range(0,60):
     time = int(input())
     if time in range(0,1001):
         min = b + time
-        if time >= 60:
-            if time % 60 == 0:
-                time == 00
-                a += 1
-                if a >= 24:
-                    a = 0
-                    print(a,min)
-                else:
-                    print(a, min)
-            elif time // 60 > 1:
-                time -= 60
-                a += time // 60
-                print(a,min)
-        else:
+        add_hour = min // 60
+        if add_hour == 0:
             print(a,min)
+        else:
+            hour = a + add_hour
+            minute = min % 60
+            if hour > 24:
+                hour -= 24
+                print(hour,minute)
+            elif hour == 24:
+                hour = 0
+                print(hour,minute)
+            else:
+                print(hour,minute)
 else:
     print('You entered wrong time range')
