2e254d29efa8e8ca3acb7fefd8aea4dffbc22903
ahnrh951019@naver.com <ahnrh951019@naver.com>
2022-04-16 11:14:08 +0900

    code: I try to make blanks

--- 단계별 풀어보기/재귀/별_찍기_10.py
+++ 단계별 풀어보기/재귀/별_찍기_10.py
@@ -5,10 +5,11 @@ star = ''
 count = n // 3
 check = 0
 add = '*'
+blank = ' '
 def make_star(count,star,check,add):
     for i in range(9):
         if i == 4:
-            star += ' '
+            star += blank
         elif i == 3 or i == 6:
             star += '\n'
             star += add
@@ -17,6 +18,7 @@ def make_star(count,star,check,add):
     print(f'star:\n{star}')
     print(f'add:\n{add}')
     add = star
+    blank = ' ' * len(add)
     check += 1
     if check == count:
         print(f'final:\n{star}')
@@ -24,3 +26,5 @@ def make_star(count,star,check,add):
         make_star(count,star,check,add)
 
 make_star(count,star,check,add)
+
+# I need a blank logic
