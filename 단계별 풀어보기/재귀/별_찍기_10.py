2aff17c36713dc70ba56a2c529ffb36de33a23e8
ahnrh951019@naver.com <ahnrh951019@naver.com>
2022-04-17 23:24:28 +0900

    code: I try to make star

--- 단계별 풀어보기/재귀/별_찍기_10.py
+++ 단계별 풀어보기/재귀/별_찍기_10.py
@@ -2,29 +2,36 @@ import sys
 
 n = int(sys.stdin.readline())
 star = ''
-count = n // 3
-check = 0
-add = '*'
-blank = ' '
-def make_star(count,star,check,add):
-    for i in range(9):
-        if i == 4:
-            star += blank
-        elif i == 3 or i == 6:
-            star += '\n'
-            star += add
-        else:
-            star += add
-    print(f'star:\n{star}')
-    print(f'add:\n{add}')
-    add = star
-    blank = ' ' * len(add)
-    check += 1
-    if check == count:
-        print(f'final:\n{star}')
-    else:
-        make_star(count,star,check,add)
+# count = n // 3
+# check = 0
+# add = '*'
+# blank = ' '
+# def make_star(count,star,check,add,blank):
+#     for i in range(9):
+#         if i == 4:
+#             star += blank
+#         elif i == 3 or i == 6:
+#             star += '\n'
+#             star += add
+#         else:
+#             star += add
+#     print(f'star:\n{star}')
+#     print(f'add:\n{add}')
+#     print(f'blank:\n{blank}')
+#     add = star
+#     blank = ' ' * len(add)
+#     check += 1
+#     if check == count:
+#         print(f'final:\n{star}')
+#     else:
+#         make_star(count,star,check,add,blank)
 
-make_star(count,star,check,add)
+# make_star(count,star,check,add,blank)
+# I have to use ['***', '* *', '***']
+def make_star(n,star):
+    for _ in range(n*n):
+        star += '*'
+    print(star)
+    
+make_star(n,star)
 
-# I need a blank logic
