13540021311cfbf65d773501b338fcc3e001bb30
Ryeohwan <ahnrh951019@naver.com>
2022-04-23 19:32:14 +0900

    code: start hanoi tower

---
+++ 단계별 풀어보기/재귀/하노이_탑_이동_순서.py
@@ -0,0 +1,19 @@
+import sys
+
+# number of plate
+n = int(sys.stdin.readline())
+
+def hanoi(n, start, middle, end):  
+    if n == 1:
+        print(start, end)
+    else:
+        hanoi(n-1,start,end,middle)
+        print(start, end)
+        hanoi(n -1, middle, start, end)
+sum = 1
+for i in range(n -1):
+    sum = sum * 2 + 1
+print(sum)
+hanoi(n, 1, 2, 3)
+
+
