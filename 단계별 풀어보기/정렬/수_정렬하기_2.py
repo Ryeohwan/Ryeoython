a98a525546ddee92b4de3dc7a722d0560d6668dc
Ryeohwan <ahnrh951019@naver.com>
2022-04-25 14:15:17 +0900

    code:i had to change type str to int

--- 단계별 풀어보기/정렬/수_정렬하기_2.py
+++ 단계별 풀어보기/정렬/수_정렬하기_2.py
@@ -3,8 +3,9 @@ n = int(sys.stdin.readline())
 list = []
 for i in range(n):
     list.append(int(input()))
-    # I have to change type str to int
 
 list.sort()
 for i in range(len(list)):
     print(list[i])
+
+# I do not know what is difference
