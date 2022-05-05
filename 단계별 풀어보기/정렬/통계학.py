beeb9a2cc3ac222df8a13bc6f91f4aa495472b37
Ryeohwan <ahnrh951019@naver.com>
2022-05-05 17:47:59 +0900

    code: add sansul

--- 단계별 풀어보기/정렬/통계학.py
+++ 단계별 풀어보기/정렬/통계학.py
@@ -1,8 +1,13 @@
 import sys
 
 n = int(sys.stdin.readline())
+list = []
+for i in range(n):
+    list.append(input())
 
 # sansul
-def sansul():
-    for i in range(n):
-        print('sansul')
+def sansul(list,n):
+    for i in range(len(list)):
+        sum += list[i]
+    return sum // n
+print(sansul(list,n))
